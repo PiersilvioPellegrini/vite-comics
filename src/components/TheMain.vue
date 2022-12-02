351a3e9a991ec65c18f86e9dcc7084778215585f
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2022-12-02 18:10:16 +0100

    add

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,7 +1,71 @@
 <script>
-export default {};
+export default {
+    data() {
+    return {
+        itemsList: [
+                {
+                    img: "buy-comics-digital-comics.png",
+                    title: "DIGITAL COMICS",
+                },
+                {
+                    img: "buy-comics-merchandise.png",
+                    title: "DC MERCHANDISE",
+                },
+                {
+                    img: "buy-comics-shop-locator.png",
+                    title: "SUBSCRIPTION",
+                },
+                {
+                    img: "buy-comics-subscriptions.png",
+                    title: "COMIC SHOP LOCATOR",
+                },
+                {
+                    img: "buy-dc-power-visa.svg",
+                    title: "DC POWER VISA",
+                },
+            ],
+            
+
+    };
+  },
+  methods: {
+  },
+};
 </script>
 
-<template></template>
+<template>
+    <section>
+        <div class=" d-flex container px-3 justify-content-between py-5">
+            <div v-for="item in itemsList " class="item d-flex align-items-center gap-4 ">
+                <img :src ="item.img" alt="">
+                <p>{{item.title}}</p> 
+            </div>
+            
+        </div>
+    </section>
+</template>
 
-<style scoped></style>
+<style scoped>
+*{
+    margin-bottom: 0px;
+}
+section{
+    background-color: #1682F9;
+    padding: 20px 0;
+}
+.item{
+    background-color: #1682F9;
+    flex-direction: row;
+    gap:20px;
+    padding:10px 10px;
+   
+}
+img{
+    display:block;
+    height: 60px;
+}
+
+p{
+    color:white;
+}
+</style>
